540d831eaf9d8e80f3bf2e6427a8cbeb0b2b8a77
evanng07 <ngoeva072@gmail.com>
2024-01-27 23:48:34 -0600

    swapped home and away team score

--- server/app.py
+++ server/app.py
@@ -7,7 +7,7 @@ from nba_api.live.nba.endpoints import boxscore
 from nba_api.stats.endpoints import playercareerstats, commonplayerinfo
 
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:3000"])
+CORS(app)
 
 
 
