60b1a736566e5e7fe8b66222e348837cda959f43
evanng07 <ngoeva072@gmail.com>
2024-01-29 16:00:23 -0600

    added navbar

--- client/src/pages/dashboard.jsx
+++ client/src/pages/dashboard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import SearchBox from './searchbar/searchBar.jsx'; 
-import Scoreboard from './scoreboard/scoreboard.jsx'
+import SearchBox from '../components/searchbar/searchBar.jsx'; 
+import Scoreboard from '../components/scoreboard/scoreboard.jsx'
 
 const Dashboard = () => {
     return (
