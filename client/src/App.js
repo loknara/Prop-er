f2fa980f10cc33598fbb021b6da7b917b78ee14d
lokeshMavs <Lokesh.Narasani@dallasmavs.com>
2024-11-07 12:27:55 -0600

    edits

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import Dashboard from "./pages/dashboard.jsx";
-import HomePage from "./pages/HomePage.jsx";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Navbar from "./components/layout/navbar.jsx";
 import executeInDevMode from "@speckai/paige";
@@ -18,7 +17,7 @@ export default function App() {
       <div className="mt-16 bg-gray-300 h-[92dvh]">
         <Routes>
           <Route path="/" element={<Dashboard />} />
-          <Route path="/home" element={<HomePage />} />
+          {/* <Route path="/home" element={<HomePage />} /> */}
           <Route path="/about" element={<Dashboard />} />
         </Routes>
       </div>
--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App';
-import './tailwind.css';
 
-ReactDOM.render(
+
+const root = createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App /> 
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />
+  </React.StrictMode>
 );
+
 
 
 
